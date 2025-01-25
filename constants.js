e4718fa942411a0ef19f5e39d70f3eccebeb2562
Anarox <68535747+Anarox1111@users.noreply.github.com>
2025-01-25 20:07:09 +0100

    Adding content to branch
    constants.js
    
    also changes in script.js

---
+++ constants.js
@@ -0,0 +1,22 @@
+/**
+ * Items and icons in the game
+ *               
+
+ * 
+ * Author:    Anarox
+ * Created:   11.05.2009
+ **/
+
+// Specials
+export const healthPotion = '<a><img src="https://cdn3.emoji.gg/emojis/3206-red-potion.png" width="64px" height="64px" alt="Red_Potion"></a>';
+
+// Special crates
+export const commonCrate = '<a><img src="https://cdn3.emoji.gg/emojis/4724-crate-uncommon.png" width="64px" height="64px" alt="crate_uncommon"></a>';
+export const rareCrate = '<a><img src="https://cdn3.emoji.gg/emojis/8446-crate-rare.png" width="64px" height="64px" alt="crate_rare"></a>';
+export const legendaryCrate = '<a><img src="https://cdn3.emoji.gg/emojis/3732-crate-legendary.png" width="64px" height="64px" alt="crate_legendary"></a>';
+
+// Special weapons
+export const blueScepter = '<a href="https://emoji.gg/emoji/9325-pole-v2"><img src="https://cdn3.emoji.gg/emojis/9325-pole-v2.png" width="64px" height="64px" alt="Pole_V2"></a>';
+export const gravityBlade = '<a href="https://emoji.gg/emoji/56060-gravity-blade"><img src="https://cdn3.emoji.gg/emojis/56060-gravity-blade.png" width="64px" height="64px" alt="Gravity_Blade"></a>';
+
+export const dragonTrident = '<a href="https://emoji.gg/emoji/4354-dragon-trident"><img src="https://cdn3.emoji.gg/emojis/4354-dragon-trident.png" width="64px" height="64px" alt="Dragon_Trident"></a>';
